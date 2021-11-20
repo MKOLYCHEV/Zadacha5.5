2a752afd0a2116b5afc3fca960c62a0a0c0d6858
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-11-20 22:43:35 +0300

    Добавьте файлы проекта.

---
+++ Zadacha5.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+            {
+                int m = n;
+                int[,] t = new int[n, m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        t[i, j] = 1 - (i + j) % 2;
+                        Console.Write("{0} ", t[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else Console.WriteLine("Ошибка! Допустимы только положительные значения.");
+            Console.ReadKey();
+        }
+    }
+}
